93d13a5ab1266d00ff3c775b447ca47903e17588
Bilal Halıcı <bila.halici@gmail.com>
2022-11-27 00:07:27 +0300

    server and client side separated

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todosSlice from "./todos/todosSlice";
-
-export const store = configureStore({
-  reducer: {
-    todos: todosSlice,
-  },
-});
